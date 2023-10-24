13630781af2bbc61865ad232cc0de3b7360effc6
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2023-10-24 19:35:15 -0400

    Update
    - Removed global variables
    - Removed lists 
    - Added os.path.join to all paths

--- image_mover.py
+++ image_mover.py
@@ -7,133 +7,87 @@ import shutil
     Simply place this file in the same directory as the images folder and run it.
 """
 
-# The following lists are used to store the image names associated with each label
-armCover = []
-otherRoverPart = []
-artifact = []
-nearbySurface = []
-closeUpRock = []
-drt = []
-drtSpot = []
-distantLandscape = []
-drillHole = []
-nightSky = []
-floats = []
-layers = []
-lightTonedVeins = []
-mastcamCalTarget = []
-sand = []
-sun = []
-wheel = []
-wheelJoint = []
-wheelTracks = []
-
-# This list is used to store images that are not labeled correctly
-trash = []
-
 
 def main():
     """
-        This function calls the labelIdentifier function on each of the three files and then calls 
-        the image_mover function on each of the lists to move the images to their appropriate folders
+        This function calls the labelIdentifier function on each of the three files that contain the image names and their labels
     """
     
-    with open('test-set-v2.1.txt', 'r') as f:
-        labelIdentifier(f)
+    with open('test-set-v2.1.txt', 'r') as file:
+        labelIdentifier(file)
         
-    with open('train-set-v2.1.txt', 'r') as f:
-        labelIdentifier(f)
+    with open('train-set-v2.1.txt', 'r') as file:
+        labelIdentifier(file)
         
-    with open('val-set-v2.1.txt', 'r') as f:
-        labelIdentifier(f)
+    with open('val-set-v2.1.txt', 'r') as file:
+        labelIdentifier(file)
         
         
-    image_mover(armCover, os.getcwd() +'\\images\\Arm cover')
-    image_mover(otherRoverPart, os.getcwd() +'\\images\\Other rover part')
-    image_mover(artifact, os.getcwd() +'\\images\\Artifact')
-    image_mover(nearbySurface, os.getcwd() +'\\images\\Nearby surface')
-    image_mover(closeUpRock, os.getcwd() +'\\images\\Close-up rock')
-    image_mover(drt, os.getcwd() +'\\images\\DRT')
-    image_mover(drtSpot, os.getcwd() +'\\images\\DRT spot')
-    image_mover(distantLandscape, os.getcwd() +'\\images\\Distant landscape')
-    image_mover(drillHole, os.getcwd() +'\\images\\Drill hole')
-    image_mover(nightSky, os.getcwd() +'\\images\\Night sky')
-    image_mover(floats, os.getcwd() +'\\images\\Float')
-    image_mover(layers, os.getcwd() +'\\images\\Layers')
-    image_mover(lightTonedVeins, os.getcwd() +'\\images\\Light-toned veins')
-    image_mover(mastcamCalTarget, os.getcwd() +'\\images\\Mastcam cal target')
-    image_mover(sand, os.getcwd() +'\\images\\Sand')
-    image_mover(sun, os.getcwd() +'\\images\\Sun')
-    image_mover(wheel, os.getcwd() +'\\images\\Wheel')
-    image_mover(wheelJoint, os.getcwd() +'\\images\\Wheel joint')
-    image_mover(wheelTracks, os.getcwd() +'\\images\\Wheel tracks')
-    image_mover(trash, os.getcwd() +'\\images\\_Trash')
-
-
-def image_mover(list, destination):
+def labelIdentifier(file):
     """
-        This function takes in a list of images and moves them to their appropriate destination folder
+        This function takes in a file and sorts the images into their respective folders based on their labels
     """
+    basepath = os.getcwd()
     
-    os.makedirs(destination, exist_ok=True)
     
-    for image in list:
-        try:
-            shutil.move(os.getcwd() + '\\images\\' + image, destination)
-            print('Moved ' + image + ' to ' + destination)
-        except:
-            pass
-        
-
-def labelIdentifier(f):
-    """
-        This function takes in a file and sorts the images into their respective lists
-    """
-    
-    for line in f:
+    for line in file:
         image, label = line.split()
         
         if label == '0':
-            armCover.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Arm cover'))
         elif label == '1':
-            otherRoverPart.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Other rover part'))
         elif label == '2':
-            artifact.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Artifact'))
         elif label == '3':
-            nearbySurface.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Nearby surface'))
         elif label == '4':
-            closeUpRock.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Close-up rock'))
         elif label == '5':
-            drt.append(image)
+            imageMover(image, os.path.join(basepath, r'images/DRT'))
         elif label == '6':
-            drtSpot.append(image)
+            imageMover(image, os.path.join(basepath, r'images/DRT spot'))
         elif label == '7':
-            distantLandscape.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Distant landscape'))
         elif label == '8':
-            drillHole.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Drill hole'))
         elif label == '9':
-            nightSky.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Night sky'))
         elif label == '10':
-            floats.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Floats'))
         elif label == '11':
-            layers.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Layers'))
         elif label == '12':
-            lightTonedVeins.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Light-toned veins'))
         elif label == '13':
-            mastcamCalTarget.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Mastcam cal target'))
         elif label == '14':
-            sand.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Sand'))
         elif label == '15':
-            sun.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Sun'))
         elif label == '16':
-            wheel.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Wheel'))
         elif label == '17':
-            wheelJoint.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Wheel joint'))
         elif label == '18':
-            wheelTracks.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Wheel tracks'))
         else:
-            trash.append(image)
+            imageMover(image, os.path.join(basepath, r'images/Unknown'))
+        
+
+def imageMover(image, destination):
+    """
+        This function takes in an image and moves them to their appropriate destination folder
+    """
+    os.makedirs(destination, exist_ok=True)
     
+    try:
+        shutil.move(os.path.join(os.getcwd(), r'images/', image) , destination)
+        print(f'Moved %s to %s' % (image, destination))
+    except:
+        pass
+        
+
 # Calls the main function if this file is run directly
 if __name__ == '__main__':
     main()
